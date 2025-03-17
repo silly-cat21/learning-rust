886d8dc7ede3489d5a22ea913659f1fb65454ac5
silly-cat21 <luboshoracek@proton.me>
2025-03-17 17:56:05 +0100

    Initial commit

---
+++ cooking/target/debug/deps/cooking-af21b7f3b1238113.d
@@ -0,0 +1,5 @@
+/home/pokus/cooking/target/debug/deps/libcooking-af21b7f3b1238113.rmeta: src/main.rs
+
+/home/pokus/cooking/target/debug/deps/cooking-af21b7f3b1238113.d: src/main.rs
+
+src/main.rs:
---
+++ cooking/target/debug/deps/cooking-b232e072a8ca503c.d
@@ -0,0 +1,5 @@
+/home/pokus/cooking/target/debug/deps/libcooking-b232e072a8ca503c.rmeta: src/main.rs
+
+/home/pokus/cooking/target/debug/deps/cooking-b232e072a8ca503c.d: src/main.rs
+
+src/main.rs:
