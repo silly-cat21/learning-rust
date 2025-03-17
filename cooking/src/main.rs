ae86e904cdf39ba4aa6f44a364d9c10032923c89
silly-cat21 <luboshoracek@proton.me>
2025-03-17 18:38:59 +0100

    pohlavi z rodneho cisla

--- cooking/src/main.rs
+++ cooking/src/main.rs
@@ -1,15 +1,23 @@
 use std::io;
 
 fn main() {
-
-    println!("Vitej v programu pro zjisteni pohlavi a data narozeni z rodneho cisla:");
-    println!("zadej sve rodne cislo ve formatu (XXXXXX/YYYY)");
+    println!("Vítej v programu pro zjištění pohlaví a data narození z rodného čísla:");
+    println!("Zadej své rodné číslo ve formátu (XXXXXX/YYYY)");
 
     let mut a = String::new();
-    io::stdin().read_line(&mut a).expect("chyba pri nacitani hodnoty.");
+    io::stdin().read_line(&mut a).expect("Chyba při načítání hodnoty.");
 
+    let a = a.trim();
 
+    let mesic_str = &a[2..4];
+    let mesic: u32 = mesic_str.parse().expect("Chyba při převodu měsíce");
 
-
+    if mesic > 50 {
+        println!("Jsi žena");
+    } else if mesic <= 50 {
+        println!("Jsi muž");
+    } else {
+        println!("Zadal jsi neplatné rodné číslo");
+    }
 
 }
